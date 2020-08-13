0f4e54320c0a0b5a54fffb26aad65fcea28b0068
Matthew Davis <38021615+mattdavis0351@users.noreply.github.com>
2020-08-13 16:13:22 -0700

    Update index.js

--- index.js
+++ index.js
@@ -7,25 +7,25 @@ const sendUserInvitation = require('./lib/sendUserInvitation')
 const commentWithStatus = require('./lib/commentWithStatus')
 
 module.exports = (app) => {
+  // Your code here
   app.log('Yay, the app was loaded!')
 
   app.on('issue_comment.created', async (context) => {
     const bot = context.isBot
     const { body } = context.payload.comment
     const containsCommand = body.includes('/add-me')
+
     try {
+      app.log('you are inside issue_comment.created')
       if (!bot && containsCommand) {
-        await sendUserInvitation(context)
-
-        await commentWithStatus(context)
-
-        // return { inviteResp, commentResp }
-      }
-      if (bot) {
-        return false
+        console.log('from index... trying to invoke sendUserInvitation')
+        const inviteResp = await sendUserInvitation(context)
+        console.log('from index... trying to invoke commentWithStatus')
+        const commentResp = await commentWithStatus(context)
+        return { inviteResp, commentResp }
       }
     } catch (error) {
-      app.log(error.message)
+      app.log(error)
     }
   })
 }
